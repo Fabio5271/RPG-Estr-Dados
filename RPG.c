9fc4da05f5b726a6fb11004b31a833d39a84895c
Fábio <89141452+Fabio5271@users.noreply.github.com>
2023-04-17 12:09:36 -0300

    Fiz o player morrer

--- RPG.c
+++ RPG.c
@@ -4,40 +4,52 @@
 #define NUM_NODES 8
 
 // Define as arestas do grafo
-int graph[NUM_NODES][NUM_NODES] = {
+int game_path[NUM_NODES][NUM_NODES] = {
     {0, 1, 0, 1, 0, 0, 0, 0},
-    {0, 0, 1, 1, 1, 0, 0, 0},
+    {0, 0, 1, 2, 2, 0, 0, 0},
     {0, 0, 0, 0, 0, 1, 0, 0},
     {0, 0, 0, 0, 1, 0, 1, 0},
-    {0, 0, 0, 0, 0, 0, 1, 1},
     {0, 0, 0, 0, 0, 0, 0, 1},
-    {0, 0, 0, 0, 1, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0}
-};
+    {0, 0, 0, 0, 0, 0, 0, 2},
+    {0, 0, 0, 0, 2, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0}};
 
 // Função que verifica se o caminho escolhido pelo usuário é válido
-int is_valid_path(int path[], int num_nodes) {
+int is_valid_path(int path[], int num_nodes)
+{
     int i;
-    for (i = 0; i < num_nodes-1; i++) {
-        if (graph[path[i]][path[i+1]] == 0) {
+    for (i = 0; i < num_nodes - 1; i++){
+        if (game_path[path[i]][path[i + 1]] == 0){
             return 0;
         }
     }
     return 1;
 }
 
-// Função que verifica se o caminho escolhido pelo usuário leva à Espada da Tormenta
-int is_path_to_sword(int path[], int num_nodes) {
-    if (path[num_nodes-1] == NUM_NODES-1) {
-        return 1;
+// verifica se o player morre e se sim volta pro começo
+int check_death(int path[], int num_nodes, int *pos){
+    if (game_path[path[num_nodes-2]][path[num_nodes-1]] != 2){ // verifica se o caminho escolhido leva à Espada da Tormenta
+        return 0;
     }
-    return 0;
+    
+    if(path[num_nodes-2] == 1 && path[num_nodes-1] == 3){
+        printf("\nEntrou na vala.");
+    }
+    if(path[num_nodes-2] == 5 && path[num_nodes-1] == 7){
+        printf("\nSeu cu ardeu.");
+    }
+
+    printf("\nVocê escolheu o caminho errado. Você morreu.\n");
+    printf("Voltando pro começo...\n");
+    *pos = 0;
+
+    return 1;
 }
 
-int main() {
+int main(){
     int current_node = 0; // começa na entrada do labirinto
     int path[NUM_NODES]; // armazena o caminho escolhido pelo usuário
-    int num_nodes_in_path = 1; // começa com um nó na lista (a entrada)
+    int nodes_in_path = 1; // numero de nos no caminho, começa com um (a entrada)
     path[0] = current_node;
 
     printf("Bem-vindo ao RPG do FUSCAO\n");
@@ -47,33 +59,32 @@ int main() {
     printf("O caminho pelo labirinto é complicado e você precisa escolher cuidadosamente qual caminho seguir para evitar as armadilhas e derrotar os monstros.");
     printf("Voce chegou a porta do labirinto:");
 
-    while (current_node != NUM_NODES - 1) { // enquanto não chegar à Espada da Tormenta
-    printf("\nVocê está no nó %d.\n", current_node + 1);
-    printf("Para onde você quer ir?\n");
+    while (current_node != NUM_NODES - 1){ // enquanto não chegar à Espada da Tormenta
+        printf("\nVocê está no nó %d.\n", current_node + 1);
+        printf("Para onde você quer ir?\n");
 
-    for (int i = 0; i < NUM_NODES; i++) {
-        if (graph[current_node][i] == 1) {
-            printf("%d. Nó %d\n", i + 1, i + 1);
+        for (int i = 0; i < NUM_NODES; i++){
+            // if (game_path[current_node][i] == 1){
+            if (game_path[current_node][i] != 0){
+                printf("%d. Nó %d\n", i + 1, i + 1);
+            }
         }
+
+        int choice;
+        scanf("%d", &choice);
+        if (choice < 1 || choice > NUM_NODES || game_path[current_node][choice - 1] == 0){
+            printf("Opção inválida. Tente novamente.\n");
+            continue;
+        }
+        if(game_path[current_node][choice-1] == 2){
+
+        }
+        path[nodes_in_path++] = choice - 1;
+        
+        current_node = choice - 1;
+        
+        check_death(path, nodes_in_path, &current_node);
     }
-
-    int choice;
-    scanf("%d", &choice);
-    if (choice < 1 || choice > NUM_NODES || graph[current_node][choice - 1] == 0) {
-        printf("Opção inválida. Tente novamente.\n");
-        continue;
-    }
-
-    path[num_nodes_in_path++] = choice - 1;
-
-    // Verifica se o caminho escolhido é válido
-    if (!is_valid_path(path, num_nodes_in_path)) {
-        printf("Você escolheu um caminho inválido. Você morreu.\n");
-        return 0;
-    }
-
-    current_node = choice - 1;
-}
-printf("\nParabéns! Você encontrou a Espada da Tormenta!\n");
-return 0;
+    printf("\nParabéns! Você encontrou a Espada da Tormenta!\n");
+    return 0;
 }

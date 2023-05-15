eaecc27a6569c41c52961c4b8b0743b7d37c767c
Fabio5271 <89141452+Fabio5271@users.noreply.github.com>
2023-05-15 11:47:17 -0300

    Mothe ajeitou a montanha

--- montanha.h
+++ montanha.h
@@ -18,19 +18,19 @@ int mount_path[NUM_NODES][NUM_NODES] = {
 // Define as mensagens de entrada em cada local
 int m_node_msg(int orig, int dest){
     if (orig == 1 && dest == 2){
-        printf("\nTu olha e tem um montanhão.");
+        printf("\nVocê se aproxima da base da montanha e sente um arrepio percorrer sua espinha. Com um suspiro, você se prepara para subir. A entrada é estreita e a rocha é áspera ao toque, mas você não se detém.");
     }
     if (orig == 1 && dest == 3){
-        printf("\n");
+        printf("\nVocê segue o caminho à sua frente e encontra um corredor estreito, com a rocha escorregadia sob seus pés. O ar é úmido e o som de gotejamento ecoa nas paredes da caverna.");
     }
     if (orig == 2 && dest == 4){
-        printf("\n");
+        printf("\nAo se virar, você vê um caminho largo que serpenteia pela montanha. O sol brilha acima, iluminando o caminho à frente. Você avança com cautela, sentindo cada fibra de seu corpo congelando");
     }
     if (orig == 3 && dest == 5){
-        printf("\n");
+        printf("\nO caminho se estreita e você começa a subir, a rocha ficando cada vez mais íngreme. Você sente o cheiro de monstros próximos e mantém sua mão firme na empunhadura da espada.");
     }
     if (orig == 5 && dest == 6){
-        printf("\n");
+        printf("\nVocê chega ao topo da montanha, exausto e ofegante. Mas a vista lá de cima compensa todo o esforço. Ele avista o tesouro reluzente, o escudo congelado. Você respira fundo e aprecia a beleza da paisagem, sentindo-se vivo e realizado por ter superado mais esse desafio");
     }
     printf("\nOque você irá fazer agora?\n");
 }
@@ -38,14 +38,14 @@ int m_node_msg(int orig, int dest){
 // Mostra as mensagens de morte de cada caminho
 int m_death_msg(int orig, int dest){
     if (orig == 3 && dest == 5){
-        printf("\nUma avalanche te engoliu.");
+        printf("\nVocê se move pela trilha e ouve um rugido vindo de um ponto mais adiante. Você aperta a espada e se prepara para o que está por vir. Quando você chega mais perto, um troll aparece em sua frente. Com um rugido, ele ataca,você luta bravamente, mas acaba sendo derrotado.");
     }
     if (orig == 4 && dest == 6){
-        printf("\nO chão quebrou e você afundou em um lago congelado.");
+        printf("\nVocê continua a subir a montanha, mas percebe que a rocha está ficando cada vez mais instável. De repente, um pedaço da rocha se solta, fazendo com que você perca o equilíbrio. Você tenta se agarrar a algo, mas não consegue, e acaba caindo para a morte.");
     }
-    printf("\nVocê escolheu o caminho errado. Você morreu congelado.\n");
+    printf("\nVocê escolheu o caminho errado. Você morreu.\n");
 }
 //Define mensagem de inicio
-const char* mount_start = "Você chegou na base da montanha congelada, para encontrar o escudo da montanha congelada é preciso chegar ao topo!\nTome muito cuidado com suas escolhas,pois o caminho é cheio de armadilhas e monstros de gelo! \n";
+const char* mount_start = "Você chegou na base da montanha congelada, para encontrar o escudo da montanha congelada é preciso chegar ao topo!. Tome muito cuidado com suas escolhas,pois o caminho é traiçoeiro e cheio de monstros! \n";
 //Define mensagem de conclusão
-const char* mount_end ="\nVocê encontrou o escudo da montanha gelada!\n";
+const char* mount_end ="\nParabéns! Você encontrou o escudo da montanha congelada!\n";
